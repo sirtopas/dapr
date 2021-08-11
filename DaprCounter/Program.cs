0ff05ccaad235ec9df3b12b5e3b8b5db1578f6bf
James <sirtopas@gmail.com>
2021-08-11 17:59:34 +0100

    chore: add .net console project

---
+++ DaprCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DaprCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
