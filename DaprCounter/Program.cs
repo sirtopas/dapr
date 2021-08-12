74c0c2f9f60828b8cd5ece1193ef7140f5c32db2
James <sirtopas@gmail.com>
2021-08-12 20:31:05 +0100

    chore: create new dapr instance to get and save to the state store

--- DaprCounter/Program.cs
+++ DaprCounter/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Dapr.Client;
 
 namespace DaprCounter
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string storeName = "statestore";
+            const string key = "counter";
+
+            var daprClient = new DaprClientBuilder().Build();
+            var counter = await daprClient.GetStateAsync<int>(storeName, key);
+
+            while (true)
+            {
+                Console.WriteLine($"Counter = {counter++}");
+
+                await daprClient.SaveStateAsync(storeName, key, counter);
+                await Task.Delay(1000);
+            }
         }
     }
 }
